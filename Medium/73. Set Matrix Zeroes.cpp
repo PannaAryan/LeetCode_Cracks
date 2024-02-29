0d49764aae79189b78180160fa7abe62eb38447e
PannaAryan <panna.codebase@gmail.com>
2024-02-29 21:36:45 +0600

    some

---
+++ Medium/73. Set Matrix Zeroes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        unordered_set<int>row;
+        unordered_set<int>column;
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]==0){
+                    row.insert(i);
+                    column.insert(j);
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(row.count(i) > 0 || column.count(j) > 0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+
+    }
+};
