c0033eb0ee9f001ffbf6e846215ba3f3a7f55a9f
PannaAryan <panna.codebase@gmail.com>
2025-02-10 19:20:22 +0600

    daily problem solved

---
+++ Easy/3174. Clear Digits.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+        
+        for(char c : s) {
+            if(isdigit(c)) {
+                if(!st.empty()) st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        
+        string ans = "";
+        while(!st.empty()) {
+            ans = st.top() + ans;
+            st.pop();
+        }
+        
+        return ans;
+    }
+};
